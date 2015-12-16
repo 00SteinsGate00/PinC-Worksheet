1021d5f79badafc7451b83fb30e74cc1565a2c2e
Jan Holub <Jan-Holub@gmx.net>
2015-12-16 12:45:36 +0100

    Das Befehlsübersichtskapitel um Programmstruktur, Includes und logische Operatoren erweitert

---
+++ CodeSnippets/Befehlsuebersicht/print_int.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int x = 10;
+	printf("%i\n", 10);
+	return 0;
+}
---
+++ CodeSnippets/Befehlsuebersicht/scan.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int x;
+	scanf("%i", &x);
+	return 0;
+}
