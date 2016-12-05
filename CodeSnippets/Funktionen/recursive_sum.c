c31e1e25a21a12d2ca44ffc8263afc84c46d964b
Jan Holub <Jan-Holub@gmx.net>
2016-12-05 11:47:54 +0100

    [Funktionen] Fehler in der GaussSummen Funktion gefixt

--- CodeSnippets/Funktionen/recursive_sum.c
+++ CodeSnippets/Funktionen/recursive_sum.c
@@ -5,7 +5,7 @@
 // Gausssumme von 5: 5 + 4 + 3 + 2 + 1 = 15
 int GaussSum(int x){
 	if(x > 1){
-		return x + sum(x-1);
+		return x + GaussSum(x-1);
 	}
 	// In diesem Fall endet die Funktion
 	else{
